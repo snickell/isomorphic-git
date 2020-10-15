e8483df2afbd0bb46d4afe48a0dd1b3b7712d7bf
Seth Nickell <snickell@gmail.com>
2020-10-14 21:38:04 -1000

    Commit WIP

--- __tests__/__helpers__/FixtureFS.js
+++ __tests__/__helpers__/FixtureFS.js
@@ -1,22 +1,28 @@
 /* eslint-env node, browser, jasmine */
 
-const { makeBrowserFS } = require('./FixtureFS/makeBrowserFS.js')
-const { makeLightningFS } = require('./FixtureFS/makeLightningFS.js')
-const { makeNodeFixture } = require('./FixtureFS/makeNodeFixture.js')
+// const { makeBrowserFS } = require('./FixtureFS/makeBrowserFS.js')
+const { makeIPFS } = require('./FixtureFS/makeIPFS.js')
+// const { makeLightningFS } = require('./FixtureFS/makeLightningFS.js')
+// const { makeNodeFixture } = require('./FixtureFS/makeNodeFixture.js')
 const setTestTimeout = require('./set-test-timeout')
 setTestTimeout(60000)
 
 async function makeFixture(dir) {
-  return process.browser ? makeBrowserFixture(dir) : makeNodeFixture(dir)
+  // return process.browser ? makeBrowserFixture(dir) : makeNodeFixture(dir)
+  return makeIPFS(dir)
 }
 
-async function makeBrowserFixture(dir) {
-  // enable / disable console.log statements
-  // window.localStorage.debug = 'isomorphic-git'
-  const isSafari = /Safari/.test(navigator && navigator.userAgent)
-  return process.env.ENABLE_LIGHTNINGFS && !isSafari
-    ? makeLightningFS(dir)
-    : makeBrowserFS(dir)
-}
+// async function makeBrowserFixture(dir) {
+//   // enable / disable console.log statements
+//   // window.localStorage.debug = 'isomorphic-git'
+//   const isSafari = /Safari/.test(navigator && navigator.userAgent)
+//   if (process.env.NO_IPFS) {
+//     return process.env.ENABLE_LIGHTNINGFS && !isSafari
+//       ? makeLightningFS(dir)
+//       : makeBrowserFS(dir)
+//   } else {
+//     return makeIPFS(dir)
+//   }
+// }
 
 module.exports.makeFixture = makeFixture
